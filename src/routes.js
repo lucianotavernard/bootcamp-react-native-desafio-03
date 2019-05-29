ce7d701612279e7601bd0621fee3d1691fe20a0a
luciano tavernard <ltms00@hotmail.com>
2019-05-28 23:14:34 -0300

    Configurado o arquivo de routes

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+import { createAppContainer, createStackNavigator } from "react-navigation";
+
+import Main from "~/pages/main";
+
+const Routes = createAppContainer(createStackNavigator({ Main }));
+
+export default Routes;
